eaaca9e6364484ca53f593ebc9f5746d5f987038
larissabrisola <alarissabrisola@gmail.com>
2024-05-10 12:52:31 -0300

    acho que agora foi e se nao foi e pq deus nao quis pois 24h nsiso socorro

--- cypress/support/steps/listUsers.step.js
+++ cypress/support/steps/listUsers.step.js
@@ -1,19 +1,17 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-import ListUsers from "../pages/listUsers.page";
 
-let listUsers = new ListUsers()
 
 Given('usuário está na tela inicial', ()=>{
     cy.visit('https://rarocrud-frontend-88984f6e4454.herokuapp.com/')
     cy.url().should('eq', 'https://rarocrud-frontend-88984f6e4454.herokuapp.com/users')
 })
 
+
 When('não existir usuários cadastrados', () => {
     cy.intercept("GET", "https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1/users", {
       statusCode: 200,
       body: [],
     }).as("emptyList");
-
   });
 
 
@@ -26,3 +24,62 @@ Then('um texto informando que não existe usuários cadastrados deverá ser exib
 })
 
 
+When('existir mais que 6 usuários cadastrados', ()=>{
+  cy.intercept('GET', 'https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1/users', {
+    statusCode: 200, 
+    fixture: 'twentyUsers.json'
+  }).as('vinteUsuariosCadastrados')
+
+  cy.visit('https://rarocrud-frontend-88984f6e4454.herokuapp.com/users')
+
+})
+
+When('existir 6 usuários cadastrados', ()=>{
+  cy.intercept('GET', 'https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1/users', {
+    statusCode: 200, 
+    fixture: 'sixUsers.json'
+  }).as('sixUsers')
+
+  cy.visit('https://rarocrud-frontend-88984f6e4454.herokuapp.com/users')
+
+})
+
+Then('a paginação deve estar disponinel', ()=>{
+  cy.wait('@vinteUsuariosCadastrados')
+  cy.get('#paginacaoAtual').should('be.visible').and('contain', '1 de 4')
+  cy.get('#paginacaoVoltar').should('be.disabled')
+  cy.get('#paginacaoProximo').should('be.enabled')
+})
+
+When('clicar no botão de Próximo', ()=>{
+  cy.get('#paginacaoProximo').click()
+})
+
+Then('deve ser capaz ir para a proxima página', ()=>{
+  cy.get('#paginacaoAtual').should('be.visible').and('contain', '2 de 4')
+  cy.get('#paginacaoVoltar').should('be.enabled')
+  cy.get('#paginacaoProximo').should('be.enabled')
+})
+
+Then('deve ser capaz de voltar a página Anterior', ()=>{
+  cy.get('#paginacaoVoltar').should('be.enabled')
+  cy.get('#paginacaoProximo').should('be.enabled')
+})
+
+Then('a paginação não deve estar disponivel', ()=>{
+  cy.wait('@sixUsers')
+  cy.get('#paginacaoAtual').should('be.visible').and('contain', '1 de 1')
+  cy.get('#paginacaoVoltar').should('be.disabled')
+  cy.get('#paginacaoProximo').should('be.disabled')
+})
+
+Then('não deve ser possivel clicar no botão de proximo', ()=>{
+  cy.get('#paginacaoProximo').should('be.disabled')
+})
+
+When('clicar no botão de próximo 3 vezes', ()=>{
+  cy.get('#paginacaoProximo').click()
+  cy.get('#paginacaoProximo').click()
+  cy.get('#paginacaoProximo').click()
+
+})
